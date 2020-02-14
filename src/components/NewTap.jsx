90651aae324d4067bac70864221ec2c9cabe270f
Jake Ash <jacob.ash1998@gmail.com>
2020-02-14 14:59:44 -0800

    Move the new tap form to center of page

--- src/components/NewTap.jsx
+++ src/components/NewTap.jsx
@@ -7,33 +7,30 @@ function StorySuggestion(){
     display: 'block'
   }
   const BoxMaker = {
-    marginLeft: '20px',
     display: 'inline-block',
     textAlign: 'left',
     marginLeft: 'auto',
     marginRight: 'auto',
-    border: '15px #FFFFFF',
     padding: '15px',
     margin: '20px',
     width: '400px',
     borderRadius: '5px',
-    position: 'absolute',
     fontFamily: 'Impact, Charcoal, sans-serif',
     color: '#484848',
     boxShadow: '0 16px 40px rgba(0,0,0,0.12)',
     backgroundColor: '#b8a98f',
     opacity: '0.85'
-    }
-    const Inline = {
-        display: 'inline-block'
-    }
-    const LabelChange = {
-        display: 'block'
-    }
-    const ButtonFloat = {
-        float: 'right',
-        fontFamily: 'Impact, Charcoal, sans-serif'
-    }
+  }
+  const Inline = {
+    display: 'inline-block'
+  }
+  const LabelChange = {
+    display: 'block'
+  }
+  const ButtonFloat = {
+    float: 'right',
+    fontFamily: 'Impact, Charcoal, sans-serif'
+  }
   return(
     <div>
       <h1><style jsx> {`
@@ -43,28 +40,28 @@ function StorySuggestion(){
       font-family: Impact, Charcoal, sans-serif;
     }
     `}</style>Add a new tap</h1>
-    <div>
-    <div style = {BoxMaker}>
-            <h1>Enter the new tap's information</h1>
-            <form>
-                <label>Brand</label>
-                <br></br>
-                <input type="text"></input>
-                <br/>
-                <br/>
-                <div style={Inline}>
-                    <label style = {LabelChange}>Type</label>
-                    <input type="text"></input>
-                </div>
-                <div style={Inline}>
-                    <label style = {LabelChange}>ABV</label>
-                    <input type="text"></input>
-                </div>
-                <br/>
-                <button type="submit" style = {ButtonFloat}>Search</button>
-            </form>
-          </div>
-    </div>
+      <div style ={BoxLocation}>
+        <div style = {BoxMaker}>
+          <h1>Enter the new tap's information</h1>
+          <form>
+            <label>Brand</label>
+            <br></br>
+            <input type="text"></input>
+            <br/>
+            <br/>
+            <div style={Inline}>
+              <label style = {LabelChange}>Type</label>
+              <input type="text"></input>
+            </div>
+            <div style={Inline}>
+              <label style = {LabelChange}>ABV</label>
+              <input type="text"></input>
+            </div>
+            <br/>
+            <button type="submit" style = {ButtonFloat}>Search</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
